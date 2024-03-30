6e59af9209709ddc01f4f4c8f0e51f7b863481d4
mariohamann <mario.hamann@virtual-identity.com>
2024-03-30 19:50:00 +0100

    fix: quick fix for unrendered component

--- resources/js/site.js
+++ resources/js/site.js
@@ -4,6 +4,7 @@ import morph from '@alpinejs/morph'
 import persist from '@alpinejs/persist'
 import focus from '@alpinejs/focus'
 import 'focus-visible'
+import '@mariohamann/activity-graph'
 
 // Global get CSRF token function (used by forms).
 window.getToken = async () => {
