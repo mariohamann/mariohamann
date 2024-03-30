10d3d2618f72ae64124e0b5420b23dfc35b3652e
mariohamann <mario.hamann@virtual-identity.com>
2024-03-30 16:24:49 +0100

    feat: add activity graph

---
+++ app/Http/Controllers/TrainingController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Statamic\Facades\GlobalSet;
+use DateTime;
+
+class TrainingController extends Controller
+{
+    public function update(Request $request)
+    {
+        // Check if API_TOKEN is set in env
+        if (!env('API_TOKEN')) {
+            return response()->json(['error' => 'API token not set'], 500);
+        }
+
+        // Check if the request has the correct token
+        $token = $request->header('Authorization');
+        if ($token !== 'Bearer ' . env('API_TOKEN')) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        // Validate the request body
+        $validated = $request->validate([
+            'data' => 'required|array',
+        ]);
+
+        $activityGlobal = GlobalSet::findByHandle('activity');
+
+        if (!$activityGlobal) {
+            return response()->json(['error' => 'Global set not found'], 404);
+        }
+
+        $trainingsData = $activityGlobal->inDefaultSite()->get('trainings', []);
+
+        foreach ($validated['data'] as $date => $uuid) {
+            $dateTime = new DateTime($date);
+
+            // Sometimes I'm getting to bed after midnight
+            // I want to count those trainings as the previous day
+            // Check if the time is between 00:00 and 04:00
+            if ((int)$dateTime->format('G') < 4) {
+                // Subtract one day
+                $dateTime->modify('-1 day');
+                // Set time to 23:59:59
+                $dateTime->setTime(23, 59, 59);
+            }
+
+            $trainingsData[$dateTime->format('Y-m-d\TH:i:sP')] = $uuid;
+        }
+
+        $activityGlobal->inDefaultSite()->set('trainings', $trainingsData)->save();
+
+        // get complete data, sort it and save it
+        $trainingsData = $activityGlobal->inDefaultSite()->get('trainings', []);
+        ksort($trainingsData);
+        $activityGlobal->inDefaultSite()->set('trainings', $trainingsData)->save();
+
+        return response()->json(['success' => 'Trainings updated successfully.']);
+    }
+}
---
+++ app/Http/Middleware/Enhance.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Symfony\Component\HttpFoundation\Response;
+use Extism\Plugin;
+use Extism\Manifest;
+use Extism\PathWasmSource;
+use Illuminate\Support\Str;
+
+class Enhance
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        // Exclude specific paths
+        if (
+            Str::startsWith($request->path(), "cp") ||
+            Str::startsWith($request->path(), "api") ||
+            !Str::contains($response->getContent(), "</activity-graph>")
+        ) {
+            return $response;
+        }
+
+        // Generate cache key based on content and file hashes
+        $wasmPath = base_path("vendor/enhance/ssr-wasm/enhance-ssr.wasm");
+        $jsPath = base_path("node_modules/@mariohamann/activity-graph/dist/activity-graph-wasm/en.min.js");
+        $cacheKey = md5($response->getContent() . filemtime($wasmPath) . filemtime($jsPath));
+
+        // Processing
+        $start = microtime(true);
+        if (Cache::has($cacheKey)) {
+            $newResponse = Cache::get($cacheKey);
+            $response->setContent($newResponse);
+        } else {
+
+            $wasm = new PathWasmSource($wasmPath);
+            $manifest = new Manifest($wasm);
+            $enhance = new Plugin($manifest, true);
+
+            $input = [
+                "markup" => $response->getContent(),
+                "elements" => [
+                    "activity-graph" => file_get_contents($jsPath),
+                ],
+            ];
+
+            $json = json_encode($input);
+            $output = $enhance->call("ssr", $json);
+            $newResponse = json_decode($output)->document;
+
+            Cache::put($cacheKey, $newResponse, now()->addDays(7));
+
+            $response->setContent($newResponse);
+        }
+
+        $end = microtime(true);
+        $execution_time = ($end - $start) * 1000;
+        $response->header("X-Enhance-Execution-Time", $execution_time);
+
+        return $response;
+    }
+}
