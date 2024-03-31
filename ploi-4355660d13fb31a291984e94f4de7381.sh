13a8f6d52f594c00ef9e76f5309eb72f1ef061e4
Mario Hamann <mariovhamanngmail.com>
2024-03-31 13:50:01 +0000

    Changes from server [no ci]

---
+++ ploi-4355660d13fb31a291984e94f4de7381.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+if [[ "feat: add cache refresher + refresh on training data change" =~ "[no ci]" ]]; then
+    echo "Skipping deployment upon commit message request."
+    exit 0
+fi
+
+cd /home/mariohamann-sljur/mariohamann.com
+git pull origin master
+
+composer install --no-interaction --prefer-dist --optimize-autoloader
+echo "" | sudo -S service php8.2-fpm reload
+
+
+npx pnpm i & npx pnpm build
+
+php artisan cache:clear
+php artisan config:cache
+php artisan route:cache
+php artisan statamic:stache:warm
+php artisan statamic:search:update --all
+php artisan statamic:static:clear
+php artisan statamic:static:warm
+php artisan statamic:assets:generate-presets
+
+echo "🚀 Application deployed!"
